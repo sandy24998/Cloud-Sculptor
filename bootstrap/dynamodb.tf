4965f12a25981abb18ae48916b9d65c2acef1e6d
Sandeep Pandit <54panditsandeep@gmail.com>
2026-06-07 16:49:31 +0000

    Added Terraform remote backend bootstrap

---
+++ bootstrap/dynamodb.tf
@@ -0,0 +1,19 @@
+resource "aws_dynamodb_table" "terraform_lock" {
+
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  tags = {
+    Name = "terraform-locks"
+  }
+}
---
+++ bootstrap/outputs.tf
@@ -0,0 +1,7 @@
+output "state_bucket" {
+  value = aws_s3_bucket.terraform_state.bucket
+}
+
+output "lock_table" {
+  value = aws_dynamodb_table.terraform_lock.name
+}
