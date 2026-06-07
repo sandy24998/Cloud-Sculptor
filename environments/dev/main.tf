3e670d0fbd185baab79baa7b4ec52eb37e000d93
Sandeep Pandit <54panditsandeep@gmail.com>
2026-06-07 17:40:16 +0000

    Added reusable VPC networking module

---
+++ environments/dev/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+
+  source = "../../modules/vpc"
+
+  vpc_name = var.vpc_name
+  vpc_cidr = var.vpc_cidr
+
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+
+  availability_zones = var.availability_zones
+}
---
+++ environments/dev/terraform.tfvars
@@ -0,0 +1,20 @@
+aws_region = "ap-south-1"
+
+vpc_name = "cloud-sculptor-dev-vpc"
+
+vpc_cidr = "10.0.0.0/16"
+
+public_subnet_cidrs = [
+  "10.0.1.0/24",
+  "10.0.2.0/24"
+]
+
+private_subnet_cidrs = [
+  "10.0.11.0/24",
+  "10.0.12.0/24"
+]
+
+availability_zones = [
+  "ap-south-1a",
+  "ap-south-1b"
+]
