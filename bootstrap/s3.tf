a0f4409550dfae50d3f802dfe1b7633c924f81ea
Sandeep Pandit <54panditsandeep@gmail.com>
2026-07-18 18:07:05 +0000

    formatting done for all terraform files

--- bootstrap/s3.tf
+++ bootstrap/s3.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket                  = aws_s3_bucket.terraform_state.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,28 +16,28 @@ resource "aws_internet_gateway" "this" {
 
 
 resource "aws_subnet" "public" {
-  count = length(var.public_subnet_cidrs)
+  count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
   tags = {
-  Name = "${var.vpc_name}-public-${count.index + 1}"
-  "kubernetes.io/role/elb" = "1"
-  "kubernetes.io/cluster/cloud-sculptor-dev-eks" = "shared"
-}
+    Name                                           = "${var.vpc_name}-public-${count.index + 1}"
+    "kubernetes.io/role/elb"                       = "1"
+    "kubernetes.io/cluster/cloud-sculptor-dev-eks" = "shared"
+  }
 }
 
 resource "aws_subnet" "private" {
-  count = length(var.private_subnet_cidrs)
-  vpc_id = aws_vpc.this.id
-  cidr_block = var.private_subnet_cidrs[count.index]
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = var.availability_zones[count.index]
   tags = {
-  Name = "${var.vpc_name}-private-${count.index + 1}"
-  "kubernetes.io/role/internal-elb" = "1"
-  "kubernetes.io/cluster/cloud-sculptor-dev-eks" = "shared"
-}
+    Name                                           = "${var.vpc_name}-private-${count.index + 1}"
+    "kubernetes.io/role/internal-elb"              = "1"
+    "kubernetes.io/cluster/cloud-sculptor-dev-eks" = "shared"
+  }
 }
 
 resource "aws_eip" "nat" {
@@ -49,7 +49,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "this" {
   allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.public[0].id
+  subnet_id     = aws_subnet.public[0].id
   depends_on = [
     aws_internet_gateway.this
   ]
@@ -66,14 +66,14 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route" "public_internet" {
-  route_table_id = aws_route_table.public.id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.this.id
+  gateway_id             = aws_internet_gateway.this.id
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(aws_subnet.public)
-  subnet_id = aws_subnet.public[count.index].id
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
@@ -85,13 +85,13 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route" "private_nat" {
-  route_table_id = aws_route_table.private.id
+  route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.this.id
+  nat_gateway_id         = aws_nat_gateway.this.id
 }
 
 resource "aws_route_table_association" "private" {
-  count = length(aws_subnet.private)
-  subnet_id = aws_subnet.private[count.index].id
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
