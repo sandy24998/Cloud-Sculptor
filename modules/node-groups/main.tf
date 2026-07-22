a3dc58019ad97433f8c911caa87e8a212df1ce10
Sandeep Pandit <54panditsandeep@gmail.com>
2026-07-22 17:49:41 +0000

    Added node groups main file along with variables for eks node groups

---
+++ modules/node-groups/main.tf
@@ -0,0 +1,31 @@
+resource "aws_eks_node_group" "this" {
+  cluster_name    = var.cluster_name
+  node_group_name = var.node_group_name
+
+  node_role_arn = var.node_role_arn
+
+  subnet_ids = var.private_subnet_ids
+
+  instance_types = var.instance_types
+
+  capacity_type = var.capacity_type
+
+  scaling_config {
+    desired_size = var.desired_size
+    min_size     = var.min_size
+    max_size     = var.max_size
+  }
+
+  labels = var.labels
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.node_group_name
+    }
+  )
+}
